3ce4257f104f0c2e4378572aa705cb18f8e53c16
Heba Omar <jtvonline@yahoo.com>
2024-02-08 22:52:04 +0300

    Task

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,43 +1,15 @@
 #!/usr/bin/env python3
-"""
-Password Encryption and Validation Module
-"""
+""" Returns a salted, hashed password, byte in string """
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """
-        Generates a salted and hashed password.
-
-        Args:
-                password (str): A string containing the plain text
-                password to be hashed.
-
-        Returns:
-                bytes: A byte string representing the salted, hashed password.
-        """
-    encoded = password.encode()
-    hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
-
-    return hashed
+    """ Returns byte string password """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Implement is_valid to validate provided password
+    matched hashed_password
     """
-        Validates whether the provided password matches the hashed password.
-
-        Args:
-                hashed_password (bytes): A byte string representing
-                the salted, hashed password.
-                password (str): A string containing the plain text
-                password to be validated.
-
-        Returns:
-                bool: True if the provided password matches the hashed
-                password, False otherwise.
-        """
-    valid = False
-    encoded = password.encode()
-    if bcrypt.checkpw(encoded, hashed_password):
-        valid = True
-    return valid
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
